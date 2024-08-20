02ad92b0582af7211c7531f3167ae56372cbcbe6
0xabin <abinbvinod@gmail.com>
2024-08-20 09:31:10 +0530

    Rust Basic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Rust Algo ");
+
+    // ----Numbers
+    let x: i8 = 5;
+    let y: i8 = 5;
+    let z = x + y;
+    println!("total : {}", z) ;
+
+    
+//    ------Bool
+   let mut test: bool = false;
+
+   test = true;
+
+   if test {
+       println!("Test is true");
+   } else {
+      
+     println!("Test is false");
+   }
+
+
+ // --- Strings  
+  let string  = String::from("String Test");
+  println!("{}", string);
+    
+
 }
+
+
+
+ 
