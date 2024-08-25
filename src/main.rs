8e7e1628ec3a54f23585a5e443c851d5e52a55c9
0xabin <abinbvinod@gmail.com>
2024-08-25 23:32:17 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,62 @@
 fn main() {
 
-    // ----Numbers
-    let x: i8 = 5;
-    let y: i8 = 5;
-    let z = x + y;
-    println!("total : {}", z) ;
+//     // ----Numbers
+//     let x: i8 = 5;
+//     let y: i8 = 5;
+//     let z = x + y;
+//     println!("total : {}", z) ;
 
     
-//    ------Bool
-   let mut test: bool = false;
+// //    ------Bool
+//    let mut test: bool = false;
 
-   test = true;
+//    test = true;
 
-   if test {
-       println!("Test is true");
-   } else {
+//    if test {
+//        println!("Test is true");
+//    } else {
       
-     println!("Test is false");
+//      println!("Test is false");
+//    }
+
+
+//  // --- Strings  
+//   let string  = String::from("String Test");
+//   println!("{}", string);
+
+
+  // for i in 0..10 {
+  //   println!("Number is {}", i);
+  // }
+
+
+   let strings = String::from("testing");
+
+
+   println!("{}", strings);
+
+
+   for strings in strings.split_whitespace() {
+       println!("{}", strings);
    }
-
-
- // --- Strings  
-  let string  = String::from("String Test");
-  println!("{}", string);
-
-
-  for i in 0..10 {
-    println!("Number is {}", i);
-  }
      
+
+
+}
+
+
+fn testing(words: String) -> String {
+
+   let  mut ans  : String = String::from("");
+    
+    for char in words.chars() {
+       ans.push_str(&char.to_string());
+       if char == ' ' {
+        break;
+       }
+    }
+
+return ans;
 
 }
 
